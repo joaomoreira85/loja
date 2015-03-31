66cd0366fb1b63bf7c1fd28b7628e2bf13a68cf5
joaomoreira85 <joaomoreira85@hotmail.com>
2015-03-31 15:10:35 -0300

    testando

---
+++ MultiMais.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MultiMais.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestMultiMais
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ MultiMais.LojaVirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MultiMais.LojaVirtual.Web.MvcApplication" Language="C#" %>
